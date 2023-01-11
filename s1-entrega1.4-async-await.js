dbe69f135ed4ad668b711176d10fa0d4dae678e6
alexmies <orpi.alex@hotmail.com>
2023-01-11 13:42:54 +0100

    1.4 Nivell1 completat

--- s1-entrega1.4-async-await.js
+++ s1-entrega1.4-async-await.js
@@ -1,4 +1,5 @@
 // *##### Nivell 1 #####
+// Ex1
 /* Crea una funció asíncrona que rebi un id d'empleat/da i imprimeixi 
 per pantalla el nom de l'empleat/da i el seu salari, usant les funcions getEmployee() 
 i getSalary() que has definit a la tasca anterior. */
@@ -63,16 +64,46 @@ const getSalary = (objEmployee) => {
 /* Crea una nova funció asíncrona que cridi a una altra que retorni una Promise que efectuï
  la seva funció resolve() DESPRES de 2 segons de la seva invocació. */
 
-const asyncf2 = async () => {
-  let promesa = new Promise((resolve, reject) => {
-    setInterval(() => {
-      resolve({ message: 'Han passat 2s i la promesa executa resolve()' });
-      return promesa;
+const asyncf2 = () => {
+  console.log('Promesa pending');
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Promesa resolta');
     }, 2000);
   });
 };
 
-(async function asyncf1() {
+const asyncf1 = async () => {
   const result = await asyncf2();
-  console.log(result.message);
-})()
+  console.log(result);
+};
+
+asyncf1();
+
+// *##### Nivell 2 #####
+// Ex1
+/* Crea una funció que retorni el doble del número que li passa com a paràmetre després de 2 segons. */
+
+// const retornaDoble = (num) => {
+//   setTimeout(() => {
+//     // console.log(2*num)
+//     return (2*num)
+//   }, 2000);
+// };
+
+// retornaDoble(4)
+
+/* Crea una altra funció que rebi tres números i calculi la suma dels seus dobles fent servir 
+la funció anterior. */
+
+// const suma3 = async (num1, num2, num3) => {
+//     // no es inmediat (síncron), així que demanem que esperi aobtenir els valors
+//     const result = await (retornaDoble(num1) + retornaDoble(num2) + retornaDoble(num3))
+//     console.log(result)
+// }
+
+// suma3(1,2,3)
+
+// *##### Nivell 3 #####
+// Ex1
+/* Força i captura tants errors com puguis dels nivells 1 i 2. */
