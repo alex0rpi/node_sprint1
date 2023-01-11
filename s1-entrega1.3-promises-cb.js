828173e51f04bc8ee9145a37bf6b699644c5b35c
alexmies <orpi.alex@hotmail.com>
2023-01-11 10:15:54 +0100

    1.3 hasta nivel 2 ex2

--- s1-entrega1.3-promises-cb.js
+++ s1-entrega1.3-promises-cb.js
@@ -21,57 +21,87 @@ diferent depenent de si la Promise es resol o no. */
 /* Crea una arrow function que rebi un paràmetre i una funció callback i li passi a 
 la funció un missatge o un altre (que s'imprimirà per consola) en funció del paràmetre rebut. */
 
-const funcio2 = (param, cb) => {
-  if (param) {
-    cb('We got the data');
-  } else {
-    cb('Data not received');
-  }
-};
+// const funcio2 = (param, cb) => {
+//   if (param) {
+//     cb('Habemus papam');
+//   } else {
+//     cb('Non habemus papam');
+//   }
+// };
 
-funcio2(false, (x) => console.log(x));
+// funcio2(true, (x) => console.log(x));
+// funcio2(false, (x) => console.log(x));
 
 // *##### Nivell 2 #####
 // Ex1
-/* Donats els objectes employees i salaries, crea una arrow function getEmployee() que retorni
- una Promise efectuant la cerca en l'objecte pel seu id. */
+/* Donats els objectes employees i salaries, crea una arrow function getEmployee() que
+retorni una Promise efectuant la cerca en l'objecte pel seu id. */
 
 let employees = [
-  {
-    id: 1,
-    name: 'Linux Torvalds',
-  },
-  {
-    id: 2,
-    name: 'Bill Gates',
-  },
-  {
-    id: 3,
-    name: 'Jeff Bezos',
-  },
+  { id: 1, name: 'Linux Torvalds' },
+  { id: 2, name: 'Bill Gates' },
+  { id: 3, name: 'Jeff Bezos' },
 ];
 
 let salaries = [
-  {
-    id: 1,
-    salary: 4000,
-  },
-  {
-    id: 2,
-    salary: 1000,
-  },
-  {
-    id: 3,
-    salary: 2000,
-  },
+  { id: 1, salary: 4000 },
+  { id: 2, salary: 1000 },
+  { id: 3, salary: 2000 },
 ];
 
-const getEmployee = () => {
-  let promesa = new Promise((resolve, reject) => {});
+const getEmployee = (id) => {
+  let found; //m'agradaria no necessitar aquesta variable
+  let promesa = new Promise((resolve, reject) => {
+    for (let i in employees) {
+      if (employees[i].id === id) {
+        found = true;
+        resolve({ message: 'Employee trobat', employeeName: employees[i].name });
+      }
+    }
+    if (!found) {
+      reject({ message: 'Employee not found' });
+    }
+  });
+  return promesa;
 };
 
-// Ex2
-// Ex3
+getEmployee(4)
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
 
+// Ex2
+/* Crea una altra arrow function getSalary() similar a l'anterior que rebi com a 
+paràmetre un objecte employee i retorni el seu salari. */
+
+const getSalary = (objEmployee) => {
+  let trobat; //m'agradaria no necessitar aquesta variable
+  let laPromesa = new Promise((resolve, reject) => {
+    for (let i in salaries) {
+      if (salaries[i].id === objEmployee.id) {
+        resolve({
+          message: 'Salary trobat',
+          empName: objEmployee.name,
+          Salary: salaries[i].salary,
+        });
+      }
+    }
+    if (!trobat) {
+      reject({ message: 'Salary not found' });
+    }
+  });
+  return laPromesa;
+};
+
+getSalary({ id: 2, name: 'Bill Gates' })
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
+
+getSalary({ id: 3, name: 'Jeff Bezos' })
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
+
+// Ex3
+/* Invoca la primera funció getEmployee() i després getSalary() niant l'execució de les dues promises de 
+manera que es retorni per la consola el nom de l'empleat/da i el seu salari. */
 // *##### Nivell 3 #####
 // Ex1
