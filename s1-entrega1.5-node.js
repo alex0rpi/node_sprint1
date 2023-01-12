6cdcbe9304bf43fb5a2c71e2700c7eed2895a447
alexmies <orpi.alex@hotmail.com>
2023-01-12 20:34:25 +0100

    1.5 nivell3 WIP

--- s1-entrega1.5-node.js
+++ s1-entrega1.5-node.js
@@ -1,38 +1,44 @@
 // *##### Nivell 1 #####
 // !Ex1---------------------------------------------------------------
 /* Crea una funció que, en executar-la, escrigui una frase en un fitxer. */
-// const fs = require('fs');
-// const { setInterval } = require('timers');
-// const path = require('path')
-// fs.writeFile('./fitxer1.txt', 'Bon dia, avui he perdut el tramvia.', () => console.log('Fitxer creat'));
+const fs = require('fs');
+const creaFitxer = (nomFitxer, text) => {
+  fs.writeFile(`./fitxers_creats/${nomFitxer}`, text, () => console.log('Fitxer creat'));
+};
+// creaFitxer('fitxer1.txt', 'Si no ho digereixo i ho practico no entenc res.');
 // --> Es crea un fitxer .txt a la mateixa arrel del repositori.
 
 // !Ex2---------------------------------------------------------------
 /*Crea una altra funció que mostri per consola el contingut del fitxer de l'exercici anterior.*/
 
-// fs.readFile('./fitxer1.txt','utf8', (err, data) => {
-//   if (err) throw err;
-//   console.log(data);
-// });
+// const fs = require('fs');
 
-// OR
-
-// fs.readFile('./fitxer1.txt', (err, data) => {
+// const mostraContingutFitxer = (fileName) => {
+//   fs.readFile(`./fitxers_creats/${fileName}`, 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+//   });
+// };
+//   // OR
+// const mostraContingutFitxer = (`fileName`) => {
+//   fs.readFile(`./fitxers_creats/${fileName}, (err, data) => {
 //     if (err) throw err;
 //     console.log(data.toString());
 //   });
+// };
+// mostraContingutFitxer('fitxer1.txt');
 
 // !Ex3---------------------------------------------------------------
 /*Crea una funció que comprimeixi el fitxer del nivell 1.*/
 // **Llibreria de node: zlib**
-// https://nodejs.org/api/zlib.html
+/* https://nodejs.org/api/zlib.html*/
+// const fs = require('fs');
 // const zlib = require('zlib');
 // const { pipeline } = require('stream');
 // const gzip = zlib.createGzip();
-// const source = fs.createReadStream('fitxer1.txt');
-// const destination = fs.createWriteStream('fitxer1.txt.gz');
+// const source = fs.createReadStream('./fitxers_creats/fitxer1.txt');
+// const destination = fs.createWriteStream('./fitxers_creats/fitxer1.txt.gz');
 
-// Utilizar una de les dues alternatives següents
 // pipeline(source, gzip, destination, (err) => {
 //   if (err) {
 //     console.error('An error occurred', err);
@@ -42,19 +48,21 @@
 //   }
 //   console.log('Compressed file was created.');
 // });
-// Alternativa més compacta que també funciona: encadenar pipes.
+
+// ALTERNATIVA: encadenar pipes.↓↓
 // source.pipe(gzip).pipe(destination)
 // hi ha vàries maneres més d'aconseguir-ho {promisify}
 
 // *##### Nivell 2 #####
 // !Ex1---------------------------------------------------------------
 /*Crea una funció que imprimeixi recursivament un missatge per la consola amb demores d'un segon.*/
-const frecursiva = () => {
-  console.log('Imprimeixo missatge cada 1s');
-  setInterval(() => {
-    frecursiva();
-  }, 1000);
-};
+// const { setInterval } = require('timers');
+// const frecursiva = () => {
+//   console.log('Imprimeixo missatge cada 1s');
+//   setInterval(() => {
+//     frecursiva();
+//   }, 1000);
+// };
 // frecursiva()
 
 // !Ex2---------------------------------------------------------------
@@ -71,15 +79,15 @@ l'ordinador utilizant Node Child Processes.*/
 //   if (err) console.log(err);
 // });
 
-// Amb node child processes
-// spawn(), fork(), exec(), execFile() --> methods to create a child process in Node.
+// Amb node child processes ↓↓
+/* spawn(), fork(), exec(), execFile() --> methods to create a child process in Node.*/
 /*child_process.exec(): spawns a shell and runs a command within that shell, passing 
 the stdout and stderr to a callback function when complete.*/
-// const child_process = require('node:child_process');
-// const { stdout, stderr } = require('process');
+const child_process = require('node:child_process');
+const { stdout, stderr } = require('process');
 
 // child_process.exec('dir directori_prova', (error, stdout, stderr) => {
-// Amb el directori d'usuari windows/users/formacio
+/*Amb el directori d'usuari windows/users/formacio*/
 // child_process.exec('dir C:\\Users\\formacio', function (error, stdout, stderr) {
 //   if (error) {
 //     console.log(`exec error: ${error}`);
@@ -92,14 +100,17 @@ the stdout and stderr to a callback function when complete.*/
 // *##### Nivell 3 ##### !Ex1
 /*Crea una funció que creï dos fitxers codificats en hexadecimal i en base64 respectivament, 
 a partir del fitxer del nivell 1.*/
-const fs = require('fs');
-const path = require('path');
 const { Buffer } = require('node:buffer');
 
-const buf = Buffer.from('fitxer1.txt', 'utf-8');
+const createCodedFiles = (file) => {
+  const buf = Buffer.from(file, 'utf-8');
+  const hexContent = buf.toString('hex')
+  const base64Content = buf.toString('base64')
+  creaFitxer('fitxerHex.txt', hexContent);
+  creaFitxer('fitxerBase64.txt', base64Content);
+};
+createCodedFiles('./fitxers_creats/fitxer1.txt')
 
-console.log(buf.toString('hex'));
-console.log(buf.toString('base64'));
 
 //!---------------------------------------------------------------
 
