213a2793f6b02613e0c04f57ae74bcac93036772
alex0rpi <orpi.alex@hotmail.com>
2023-01-20 17:27:35 +0100

    Peer to Peer amb el Tompas aplicat

--- s1-entrega1.4-async-await.js
+++ s1-entrega1.4-async-await.js
@@ -4,6 +4,9 @@
 per pantalla el nom de l'empleat/da i el seu salari, usant les funcions getEmployee() 
 i getSalary() que has definit a la tasca anterior. */
 
+/* Comentari: el Tomàs m'ha recordat la funcionalitat VSC d'executar amb F5 o start debugging
+i anar pas per pas en l'execució amb la tecla F11. */
+
 let employees = [
   { id: 1, name: 'Linux Torvalds' },
   { id: 2, name: 'Bill Gates' },
@@ -17,32 +20,39 @@ let salaries = [
 ];
 
 const getEmployee = (id) => {
-  let promesa = new Promise((resolve, reject) => {
-    if (typeof id !== 'number')
-      reject(console.log('REJECTED: Error, id received is not of type number'));
+  /* A suggerència del Tomàs, retorno directament la Promise, abans feia
+   "let promesa = new Promise" i la retornava abaix de tot. */
+  return new Promise((resolve, reject) => {
+    if (typeof id !== 'number') {
+      console.log('REJECTED: Error, id received is not of type number');
+      reject('REJECTED: Error, id received is not of type number');
+      return;
+    }
     for (let i in employees) {
       if (employees[i].id === id) {
         resolve(employees[i]);
+        return;
       }
     }
-    reject({ message: 'Employee not found' });
+    console.log('REJECTED: employee not found');
+    reject('Employee not found');
   });
-  return promesa;
 };
 
 const getSalary = (objEmployee) => {
-  let laPromesa = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     for (let i in salaries) {
       if (salaries[i].id === objEmployee.id) {
         resolve({
           name: objEmployee.name,
           salary: salaries[i].salary,
         });
+        return;
       }
     }
-    reject(console.log('REJECTED: Salary not found'));
+    console.log('REJECTED: Salary not found');
+    reject('REJECTED: Salary not found');
   });
-  return laPromesa;
 };
 
 const funcioAsync = async (idEmpleat) => {
@@ -50,8 +60,7 @@ const funcioAsync = async (idEmpleat) => {
   const { salary } = await getSalary({ id, name });
   console.log({ name, salary });
 };
-
-// funcioAsync(2);
+funcioAsync(3);
 
 // !Ex2
 /* Crea una nova funció asíncrona que cridi a una altra (funció) que retorni una Promise que efectuï
@@ -72,7 +81,7 @@ const asyncf1 = async () => {
   const result = await asyncf2();
   console.log(result);
 };
-
+// Descomentar línia següent ⬇⬇
 // asyncf1();
 
 // *##### Nivell 2 #####
@@ -82,7 +91,9 @@ const asyncf1 = async () => {
 const retornaDoble = (num) => {
   let promesa = new Promise((resolve, reject) => {
     if (!num || typeof num !== 'number') {
-      reject(console.log('REJECTED: Error, parameter either missing or not of type number'));
+      reject(
+        console.log('REJECTED: Error, parameter(s) either missing or not of type number')
+      );
       return;
     }
     setTimeout(() => {
@@ -92,7 +103,7 @@ const retornaDoble = (num) => {
   });
   return promesa;
 };
-
+// Descomentar línia següent ⬇⬇
 // retornaDoble(3);
 
 /* Crea una altra funció que rebi tres números i calculi la suma dels seus dobles fent servir
@@ -100,12 +111,12 @@ la funció anterior. */
 
 const suma3 = async (num1, num2, num3) => {
   // la funció retornaDoble() només accepta UN sol número per crida.
-  // retornaDoble() retorna una promesa, la resolució de la qual l'esperem mitjançant await.
+  // retornaDoble() retorna una promesa, la resolució de la qual l'esperem mitjançant "await".
   const resultat =
     (await retornaDoble(num1)) + (await retornaDoble(num2)) + (await retornaDoble(num3));
   console.log(`Result is ${resultat}`);
 };
-
+// Descomentar línia següent ⬇⬇
 // suma3(1, 2, 3);
 
 // *------------------------------------------------------------------------------------------
@@ -115,14 +126,14 @@ const suma3 = async (num1, num2, num3) => {
 // !Nivell1 - Ex1
 
 // Casos provats
-// Introduir una string en comptes d'un id number.
-funcioAsync('a');
+// Introduir una string en comptes d'un id number. Descomentar línia següent ⬇⬇
+// funcioAsync('a');
 
-// Indroduir un id no trobable.
+// Indroduir un id no trobable. Descomentar línia següent ⬇⬇
 // funcioAsync(9);
 
-// No passar cap paràmetre
-// funcioAsync("");
+// No passar cap paràmetre. Descomentar línia següent ⬇⬇
+// funcioAsync('');
 
 // !Nivell1 - Ex1
 /* Crec que no es poden forçar errors aquí perquè cridem una funció sense cap paràmetre que pugui induir a error. */
@@ -131,13 +142,15 @@ funcioAsync('a');
 /* Crea una funció que retorni el doble del número que li passa com a paràmetre després de 2 segons. */
 /* Aquí he forçat errors a l'hora de cridar la funcio retornaDoble. No li he vist sentit fer servir 
 try/catch en aquí ja que tinc la promesa que farà reject en cas de paràmetre no vàlid. */
+// Descomentar línia següent ⬇⬇
 // retornaDoble('a');
+// Descomentar línia següent ⬇⬇
 // retornaDoble();
 
 /* Crea una altra funció que rebi tres números i calculi la suma dels seus dobles fent servir
 la funció anterior. */
 
-//Afegeixo un bloc try catch
+//Afegeixo un bloc try catch a la funcio suma3 i li canvio el nom
 const suma3again = async (num1, num2, num3) => {
   try {
     const resultat =
